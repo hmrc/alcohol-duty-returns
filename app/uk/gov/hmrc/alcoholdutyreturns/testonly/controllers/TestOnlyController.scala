a1d2ea5096e405b43e935bb03a3f261ad66311af
matthew-goh <186605436+matthew-goh@users.noreply.github.com>
2025-01-30 15:24:47 +0000

    ADR-1687 Test only method to create user answers given regimes, without getting subscription and obligation

--- app/uk/gov/hmrc/alcoholdutyreturns/testonly/controllers/TestOnlyController.scala
+++ app/uk/gov/hmrc/alcoholdutyreturns/testonly/controllers/TestOnlyController.scala
@@ -16,16 +16,25 @@
 
 package uk.gov.hmrc.alcoholdutyreturns.testonly.controllers
 
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.alcoholdutyreturns.controllers.actions.{AuthorisedAction, CheckAppaIdAction}
+import uk.gov.hmrc.alcoholdutyreturns.models.AlcoholRegime._
+import uk.gov.hmrc.alcoholdutyreturns.models.ObligationStatus.{Fulfilled, Open}
+import uk.gov.hmrc.alcoholdutyreturns.models.{AlcoholRegime, ApprovalStatus, ObligationData, ReturnAndUserDetails, SubscriptionSummary, UserAnswers}
 import uk.gov.hmrc.alcoholdutyreturns.repositories.UserAnswersRepository
 import uk.gov.hmrc.alcoholdutyreturns.service.LockingService
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
+import java.time.LocalDate
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 class TestOnlyController @Inject() (
   cc: ControllerComponents,
+  authorise: AuthorisedAction,
+  checkAppaId: CheckAppaIdAction,
   userAnswersRepository: UserAnswersRepository,
   lockingService: LockingService
 )(implicit ec: ExecutionContext)
@@ -37,4 +46,75 @@ class TestOnlyController @Inject() (
       _ <- lockingService.releaseAllLocks()
     } yield Ok("All data cleared")
   }
+
+  def createUserAnswers(regimes: String): Action[JsValue] =
+    authorise(parse.json).async { implicit request =>
+      withJsonBody[ReturnAndUserDetails] { returnAndUserDetails =>
+        val returnId = returnAndUserDetails.returnId
+        val appaId   = returnId.appaId
+
+        Try(getRegimeFlags(regimes)) match {
+          case Success((noOFP, noSpirits, noWine, noCiderNorPerry, noBeer)) =>
+            checkAppaId(appaId).invokeBlock[JsValue](
+              request,
+              { implicit request =>
+                lockingService
+                  .withLock(returnId, request.userId) {
+                    val alcoholRegimes      = flagsToApprovalTypes(noOFP, noSpirits, noWine, noCiderNorPerry, noBeer)
+                    val subscriptionSummary = SubscriptionSummary(ApprovalStatus.Approved, alcoholRegimes)
+                    val obligationData      = getObligationData(returnId.periodKey, LocalDate.now())
+                    val userAnswers         =
+                      UserAnswers.createUserAnswers(returnAndUserDetails, subscriptionSummary, obligationData)
+                    userAnswersRepository.add(userAnswers).map { userAnswers =>
+                      Created(Json.toJson(userAnswers))
+                    }
+                  }
+                  .map {
+                    case Some(result) => result
+                    case None         => Locked
+                  }
+              }
+            )
+          case Failure(e)                                                   => Future.successful(BadRequest(e.getMessage))
+        }
+      }
+    }
+
+  private def getObligationData(periodKey: String, now: LocalDate): ObligationData = ObligationData(
+    status = Open,
+    fromDate = now,
+    toDate = now.plusDays(1),
+    dueDate = now.plusDays(2),
+    periodKey = periodKey
+  )
+
+  private def getRegimeFlags(flagDigits: String) =
+    if (!flagDigits.matches("^\\d{2}$")) {
+      throw new RuntimeException("Invalid flag digits to specify regimes")
+    } else {
+      val flags = flagDigits.toInt
+
+      val noOFP           = (flags & 0x40) != 0
+      val noSpirits       = (flags & 0x08) != 0
+      val noWine          = (flags & 0x04) != 0
+      val noCiderNorPerry = (flags & 0x02) != 0
+      val noBeer          = (flags & 0x01) != 0
+
+      (noOFP, noSpirits, noWine, noCiderNorPerry, noBeer)
+    }
+
+  private def flagsToApprovalTypes(
+    noOFP: Boolean,
+    noSpirits: Boolean,
+    noWine: Boolean,
+    noCiderNorPerry: Boolean,
+    noBeer: Boolean
+  ): Set[AlcoholRegime] =
+    Set(
+      Some(Beer).filterNot(_ => noBeer),
+      Some(Cider).filterNot(_ => noCiderNorPerry),
+      Some(Wine).filterNot(_ => noWine),
+      Some(Spirits).filterNot(_ => noSpirits),
+      Some(OtherFermentedProduct).filterNot(_ => noOFP)
+    ).flatten
 }
