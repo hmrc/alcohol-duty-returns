68b7b365a2f32135b76cc3fdfc51bd961495128d
matthew-goh <186605436+matthew-goh@users.noreply.github.com>
2025-10-02 16:44:03 +0100

    ADR-2266 Version updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.1.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val bootstrapVersion = "10.2.0"
+  private val hmrcMongoVersion = "2.9.0"
   private val mockitoScalaVersion      = "2.0.0"
 
   val compile = Seq(
@@ -12,7 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVersion,
     "org.typelevel"     %% "cats-core"                 % "2.13.0",
     "com.beachape"      %% "enumeratum-play"           % "1.9.0",
-    "com.networknt"     % "json-schema-validator"      % "1.5.8" exclude("org.slf4j", "slf4j-api")
+    "com.networknt"     % "json-schema-validator"      % "1.5.9" exclude("org.slf4j", "slf4j-api")
       exclude("com.fasterxml.jackson.core", "jackson-databind"),
   )
 
